34d69ef16d0daeef55212f90c84134f65272a8e3
JamesU <dreamingboat@outlook.com>
2021-10-30 22:54:19 +0800

    add a demo page for testing the plugin

---
+++ test/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import VideoPlayer from "components/VideoPlayer.vue"
+</script>
+
+<template>
+  <div id="left" class="side"><VideoPlayer src="./assets/cloudstream.mp4"></VideoPlayer></div>
+  <div id="right" class="side"><VideoPlayer src="./assets/windwave.mp4"></VideoPlayer></div>
+</template>
+
+<style lang="scss">
+body {
+  height: 100vh;
+  margin: 0;
+  overflow: hidden;
+  #app {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
---
+++ test/components/VideoPlayer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="frame"><video src="{{src}}"></video></div>
+  <div id="control"></div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+export default defineComponent({
+  name: "VideoPlayer",
+  props: {
+    src: {
+      type: String,
+      required: true
+    }
+  },
+  mounted() {
+  },
+  methods: {}
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>

5a3143b33cbf2da00612f482d9fb42f3941e3f7d
JamesU <dreamingboat@outlook.com>
2021-10-31 23:19:06 +0800

    add icon path variables

--- test/common/public.ts
+++ test/common/public.ts
@@ -2,3 +2,9 @@
 
 // Path of the assets folder from the root.
 export const ASSETS_PATH = "/test/assets/"
+
+// Play and pause icon path
+export const ICON_PATH = {
+  play: ASSETS_PATH + "play.svg",
+  pause: ASSETS_PATH + "pause.svg"
+}

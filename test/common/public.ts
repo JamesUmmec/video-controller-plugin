1e524b4249e52a2e3763c07a9b5e43f89687258e
JamesU <dreamingboat@outlook.com>
2021-10-31 23:37:04 +0800

    add video playing judgement prototype.

--- test/common/public.ts
+++ test/common/public.ts
@@ -1,10 +1,25 @@
 // This file contains const variables that will be used in various vue components.
 
-// Path of the assets folder from the root.
+/**
+ * Path of the assets folder from the root.
+ */
 export const ASSETS_PATH = "/test/assets/"
 
-// Play and pause icon path
+/**
+ * Play and pause icon path
+ */
 export const ICON_PATH = {
   play: ASSETS_PATH + "play.svg",
   pause: ASSETS_PATH + "pause.svg"
+}
+
+/**
+ * Init a prototype of HTMLMediaElement of whether it is playing.
+ */
+export function defineMediaPlayingJudgement() {
+  Object.defineProperty(HTMLMediaElement.prototype, 'isPlaying', {
+    get: function(){
+      return (this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2)
+    }
+  })
 }

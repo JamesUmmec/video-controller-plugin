cb3aca3dde255bf1a8fb15714cb49badc956faaf
JamesU <dreamingboat@outlook.com>
2021-10-31 20:26:42 +0800

    enable video path

--- test/App.vue
+++ test/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import VideoPlayer from "components/VideoPlayer.vue"
+import VideoPlayer from "./components/VideoPlayer.vue"
 </script>
 
 <template>
-  <div id="left" class="side"><VideoPlayer src="./assets/cloudstream.mp4"></VideoPlayer></div>
-  <div id="right" class="side"><VideoPlayer src="./assets/windwave.mp4"></VideoPlayer></div>
+  <div id="left" class="side"><VideoPlayer src="cloudstream.mp4"></VideoPlayer></div>
+  <div id="right" class="side"><VideoPlayer src="windwave.mp4"></VideoPlayer></div>
 </template>
 
 <style lang="scss">
@@ -12,9 +12,17 @@ body {
   height: 100vh;
   margin: 0;
   overflow: hidden;
+
   #app {
     width: 100%;
     height: 100%;
+
+    #right {right: 0;}
+    .side {
+      position: absolute;
+      height: 100%;
+      width: 50%;
+    }
   }
 }
 </style>
--- test/components/VideoPlayer.vue
+++ test/components/VideoPlayer.vue
@@ -1,16 +1,25 @@
 <template>
-  <div id="frame"><video src="{{src}}"></video></div>
+  <div id="frame"><video v-bind:src="videoPath"></video></div>
   <div id="control"></div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
+
+// Path from the root to the assets folder of the test page.
+const ASSETS_PATH = "/test/assets/"
+
 export default defineComponent({
   name: "VideoPlayer",
   props: {
     src: {
       type: String,
       required: true
+    }
+  },
+  data() {
+    return {
+      videoPath: ASSETS_PATH + this.src
     }
   },
   mounted() {
@@ -20,5 +29,15 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
-
+#frame {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+}
+#control {
+  position: absolute;
+  width: 100%;
+  height: 3.5rem;
+  bottom: 1rem;
+}
 </style>

5e5abd22fc25a8712ac6138bac73ac7731fe4372
JamesU <dreamingboat@outlook.com>
2021-10-31 20:42:06 +0800

    ts style without semicolon

--- test/components/VideoPlayer.vue
+++ test/components/VideoPlayer.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import {defineComponent} from "vue"
 
 // Path from the root to the assets folder of the test page.
 const ASSETS_PATH = "/test/assets/"

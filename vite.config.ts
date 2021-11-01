23a79711c5325d17a404a26f8884e859c691988d
JamesU <dreamingboat@outlook.com>
2021-11-01 20:48:38 +0800

    config vite for view raw demo page or with plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,34 @@
-import { defineConfig } from 'vite'
+import {BuildOptions, defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/main.ts"),
-      name: "videoController",
-      fileName: (format) => `plugin.${format}.js`
-    },
-    outDir: path.resolve(__dirname, "plugin"),
-    emptyOutDir: true
-  },
+/**
+ * Only the plugin can build in library mode.
+ * Just use the demo page in dev mode as a demo in vite server.
+ */
+export default defineConfig(({command, mode}) => {
+  /** Open different root path for viewing raw demo page or testing the plugin. */
+  let rootPagePath = ""
 
-  // open the demo page in the server
-  server: {
-    open: "/test/plugin.html"
+  if(mode === "raw") {
+    rootPagePath = "/test/raw.html"
+  } else if (mode === "plugin") {
+    rootPagePath = "/test/plugin.html"
+  }
+
+  return {
+    plugins: [vue()],
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, "src/main.ts"),
+        name: "videoController",
+        fileName: (format) => `plugin.${format}.js`
+      },
+      outDir: path.resolve(__dirname, "plugin"),
+      emptyOutDir: true
+    },
+    server: {
+      open: rootPagePath
+    }
   }
 })

d160ce5daff0362f731c697d36038fe3ca23f439
JamesU <dreamingboat@outlook.com>
2021-11-02 12:21:03 +0800

    add common configs

---
+++ src/common/config.ts
@@ -0,0 +1,7 @@
+// Configuration file that contains default parameters for convenient edit.
+
+/**
+ * Delay for judge mousedown or click.
+ * Attention: unit is millisecond !!!
+ */
+export const MOUSEDOWN_DELAY = 200

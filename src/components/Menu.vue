347ae6a37b7350330ee0eb5cf4cea88121ac7633
JamesU <dreamingboat@outlook.com>
2021-11-02 12:21:21 +0800

    add components

---
+++ src/components/Menu.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue"
+
+export default defineComponent({
+  name: "Menu",
+  methods: {
+    init() {}
+  }
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/VideoController.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue"
+
+export default defineComponent({
+  name: "VideoController"
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>

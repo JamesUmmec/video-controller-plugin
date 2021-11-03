b55ba503d183a0e5cece03e763fc65770b6f096b
JamesU <dreamingboat@outlook.com>
2021-11-03 19:02:02 +0800

    avoid show in page without videos

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// Create an element into the document and mount the root onto this element.
-let plugin = document.createElement("div")
-document.body.appendChild(plugin)
-createApp(App).mount(plugin)
+let mounted = false
+// Check every second, if there are video in the dom, then launch the plugin.
+setInterval(() => {
+  if (document.getElementsByTagName("video").length > 0 && !mounted) {
+    mounted = true
+
+    // Create an element into the document and mount the root onto this element.
+    let plugin = document.createElement("div")
+    document.body.appendChild(plugin)
+    createApp(App).mount(plugin)
+  }
+}, 1000)
